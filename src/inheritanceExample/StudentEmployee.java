194fc4fe6041e8331851421c4d333ffd7ee50ccc
yiqun huang <yhuang82@nyit.edu>
2022-09-29 19:59:10 -0700

    initial version

---
+++ src/inheritanceExample/StudentEmployee.java
@@ -0,0 +1,31 @@
+package inheritanceExample;
+
+public class StudentEmployee extends Student{
+    private double rateOfPayPerHour;
+    private String employeeId;
+    public StudentEmployee(String firstName, String lastName, String studentId, double rateOfPayPerHour, String employeeId){
+        super(firstName, lastName, studentId);
+        this.employeeId = employeeId;
+        this.rateOfPayPerHour = rateOfPayPerHour;
+    }
+
+    public double getRateOfPayPerHour(){
+        return rateOfPayPerHour;
+    }
+    public void setRateOfPayPerHour(double rateOfPayPerHour){
+        this.rateOfPayPerHour = rateOfPayPerHour;
+    }
+
+    public  String getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId){
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " employee ID " + employeeId + " pay " + rateOfPayPerHour;
+    }
+}
